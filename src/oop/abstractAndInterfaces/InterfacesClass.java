772fbeb5de69bc47ea558f32622a174fee83a688
Ahmed Naser <ahmednaser7897@gmail.com>
2026-07-14 12:00:01 +0300

    add Interfaces class

--- src/oop/abstractAndInterfaces/InterfacesClass.java
+++ src/oop/abstractAndInterfaces/InterfacesClass.java
@@ -1,17 +1,43 @@
 package oop.abstractAndInterfaces;
 
+import java.util.Arrays;
+import java.util.Date;
+
 public class InterfacesClass {
     public static void main(String[] args) {
+        //testInterfaces();
+        testComparableInterface();
+    }
+    public static void testInterfaces(){
+        System.out.println("Test Interfaces");
         Object[] objects = {new Tiger(), new Chicken(), new Apple()};
-        for (int i = 0; i < objects.length; i++) {
-            if (objects[i] instanceof Edible)
-                System.out.println(((Edible)objects[i]).howToEat());
+        for (Object object : objects) {
+            if (object instanceof Edible)
+                System.out.println(((Edible) object).howToEat());
 
-            if (objects[i] instanceof Animal) {
-                System.out.println(((Animal)objects[i]).sound());
+            if (object instanceof Animal) {
+                System.out.println(((Animal) object).sound());
             }
         }
+
     }
+    public static void testComparableInterface(){
+        System.out.println("Test Comparable Interface");
+        //arr wrapper class implement comparable interface
+        System.out.println(Integer.valueOf(3).compareTo(new Integer(5)));
+        System.out.println("ABC".compareTo("ABE"));
+        Date date1 = new Date(2013, 1, 1);
+        Date date2 = new Date(2012, 1, 1);
+        System.out.println(date1.compareTo(date2));
+
+        // we can use Arrays.sort to sort any class that implement comparable
+        Circle2[] list={new Circle2(3),new Circle2(2),new Circle2(5)};
+        Arrays.sort(list);
+        for (Circle2 circle : list) {
+            System.out.println(circle.getRadius());
+        }
+    }
+
 }
 
 abstract class Animal {
@@ -55,3 +81,18 @@ class Orange extends Fruit {
         return "Orange: Make orange juice";
     }
 }
+
+class Circle2  implements  Comparable<Circle2>{
+    private double radius;
+    Circle2(double radius){
+       this.radius=radius;
+    }
+    public double getArea() {return Math.PI * radius * radius;}
+    public void setRadius(double radius) {this.radius = radius;}
+    public double getRadius() {return radius;}
+    @Override
+    public int compareTo(Circle2 o) {
+        return Double.compare(this.getArea(), o.getArea());
+    }
+}
+
